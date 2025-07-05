2ce0185fc62ba34eefafc1840c2a93a61def6131
justin <yjliu1@asu.edu>
2025-07-04 18:30:07 -0700

    Added Requirements.txt

--- amenities.py
+++ amenities.py
@@ -4,7 +4,7 @@ from typing import Dict
 '''Parse 'Amenities Matrix' Sheet from New Database. Return an array of arrays of the lodge names (lodge names were orignally column B of the Amenities Matrix sheet)
 Return the first 12 lodge names for testing '''
 def get_lodge_names():
-    lodge_dataframe = pd.read_excel("C:/Users/Stanl/Downloads/New Database.xlsx", sheet_name = "Amenities Matrix", usecols = "B")
+    lodge_dataframe = pd.read_excel("New Database.xlsx", sheet_name = "Amenities Matrix", usecols = "B")
     lodges = lodge_dataframe.values.tolist()
 
     '''Remove #REF! values'''
@@ -15,7 +15,7 @@ def get_lodge_names():
             continue
         del lodges[index]
     
-    return lodges[:3]
+    return lodges
 
 '''#Create a new CSV file with the confirmed amenities with respect to the lodge names'''
 def create_amenities_dataframe(dictionary: Dict[str, Dict[str, str]]):
@@ -27,13 +27,13 @@ def create_amenities_dataframe(dictionary: Dict[str, Dict[str, str]]):
         amenity = dictionary[key]
         for amen in amenity:
             result = amenity[amen]
-            if result[0] == ' ':
-                list_of_confirmations.append(' ')
-            else:
-                total = result[0] + ' - ' + result[1] + ' from ' + result[2]
+            if result[0] == 'x':
+                total = 'x'
                 list_of_confirmations.append(total)
+            else:
+                list_of_confirmations.append(' ')
         dictionary_to_dataframe[key] = list_of_confirmations
-
+    print(dictionary_to_dataframe)
     dataframe = pd.DataFrame(dictionary_to_dataframe)
     dataframe = dataframe.transpose()
     dataframe.index.name = 'Lodge Name'
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import os
-import sys
 import time
 import ast
 import random
-import re
 from groq import Groq, RateLimitError
-
 from amenities import get_lodge_names, create_amenities_dataframe
 from requirements import get_prompt
 
@@ -27,83 +24,63 @@ def receive(lodge):
                 "content": "Does the lodge - " + lodge + " - contain the following amenities: swimming pool, waterhole, starbed, plunge pool, private terrace, landscape view, river view, fenced, not fenced, kid's club, spa, bar, beachfront, beach access, camp fire, buffet, A La Carte, Family Room, 24h Power, Inside the park"
             }
         ],
-        model="compound-beta",
+        model="llama-3.1-8b-instant",
     )
 
     return chat_completion
 
 def main():
-
+    
     lodges = get_lodge_names()
+    print(lodges)
     result = {}
 
     '''Find confirmed amenities of every lodge'''
     for i in range(0, len(lodges)):
         
         '''Variables to retry API if API calls fail'''
-        current_tries = 1
-        MAX_RETRIES = 10
+        current_tries = 0
+        MAX_RETRIES = 6
         DELAY = 1
         error = False
 
-        while current_tries < MAX_RETRIES:
+        while True:
 
             confirmed = {}   
 
             #'''Convert the response into a dictionary. Should the conversion of string to dictionary result in SyntaxError, we will go to the except block'''
             try:
-
                 #'''Ask the AI for a response & convert to dictionary'''
                 confirmed_amenities = receive(lodges[i][0]).choices[0].message.content
                 confirmed = ast.literal_eval(confirmed_amenities)
                 print("done")
                 break
             
+            #If we hit our rate limit, stop because our free tier has ended (for 24 hours)
             except RateLimitError as e:
                 error = True
                 break
-                # msg = (str(e))
-                # print(msg)
-                # match = re.search(r'try again in (\d+)m(\d+(?:\.\d+)?)s', msg)
-                # if match:
-                #     min = int(match.group(1))
-                #     sec = float(match.group(2))
-                #     total_sleep_time = (min * 60) + sec + 60
-                #     time.sleep(total_sleep_time)
-                # continue
 
             #'''If conversion from string to dictionary fails, retry until the syntax is right or we've retried up to 11 times'''
             except SyntaxError:
-                no_syntax_error = True
-                stop = False
                 current_tries = current_tries + 1
 
-                while no_syntax_error and current_tries < MAX_RETRIES:
-                    time.sleep(DELAY * (2 ** current_tries) * random.uniform(0, 0.5))
-                    print("Overwhelmed")
-                    try:
-                        c = receive(lodges[i][0]).choices[0].message.content
-                        confirmed = ast.literal_eval(c)
-                        no_syntax_error = False
-                        stop = True
-                    except SyntaxError:
-                        current_tries = current_tries + 1
-                        continue
-
-                '''If the API call was successful, break out of the while loop'''
-                if stop:
+                if current_tries > MAX_RETRIES:
+                    error = True
                     break
+                time.sleep(DELAY * (2 ** current_tries) * random.uniform(0, 0.5))
 
         if error: 
-            print("error")
+            print("Resource Exhausted")
             break
 
         '''Add Lodge: Amenities'''
         result[lodges[i][0]] = confirmed
-        time.sleep(DELAY * (2 ** current_tries) * random.uniform(0, 0.5))
-   
+        if i == 30:
+            break
 
     '''Create the new Dataframe from our results'''
+    print(result)
     create_amenities_dataframe(result)
 
 if __name__ == '__main__':
